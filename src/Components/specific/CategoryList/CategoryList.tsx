af756d19d012c9ba9ce3cb3ef854aae898b84676
monthem <lpohe01@gmail.com>
2021-09-07 14:35:41 +0900

    MISC: add transition animation to Home screen CategoryItems

--- src/Components/specific/CategoryList/CategoryList.tsx
+++ src/Components/specific/CategoryList/CategoryList.tsx
@@ -1,23 +1,34 @@
+import { config, useTransition } from "@react-spring/core";
 import React from "react";
 import styled from "styled-components";
+import { a } from "react-spring";
 import { TriviaCategory } from "../../../Interfaces/Category";
 import { Col, Row } from "../../../Styled/Grid";
 import CategoryItem from "./CategoryItem";
 
 const CategoryList = (props: CategoryListProps) => {
+
   if (!props.items.length) return (
     <div>로딩중</div>
   );
 
+  const appendedRandom = [{id: -1, name: "Random"}, ...props.items]
+
+  const transitions = useTransition(appendedRandom, {
+    from: { scale: 0, width: "100%", height: "100%", opacity: 0 },
+    enter: { scale: 1, opacity: 1 },
+    trail: 100,
+    config: config.wobbly,
+  })
+
   return (
     <Row>
-      <Col sm={6} md={4} lg={3}>
-        <CategoryItem id={-1} name={"Random"} />
-      </Col>
-      {props.items.map((item) => {
+      {transitions((spring, item) => {
         return (
           <Col sm={6} md={4} lg={3} key={item.id}>
-            <CategoryItem {...item} key={item.id} />
+            <a.div style={spring}>
+              <CategoryItem {...item} key={item.id} />              
+            </a.div>
           </Col>
         )
       })}
